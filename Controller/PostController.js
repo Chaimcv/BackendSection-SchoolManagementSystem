6f246b6e6c3425c68b114b5dc22226d7fc482fa5
Chaimcv <chaithanyamcv@gmail.com>
2026-04-20 17:23:20 +0530

    updates

--- Controller/PostController.js
+++ Controller/PostController.js
@@ -10,17 +10,30 @@ exports.createPost = async (req, res) => {
     let imageUrl = "";
 
     if (req.file) {
-      const result = await cloudinary.uploader.upload(req.file.path);
-      imageUrl = result.secure_url;
+      const result = await cloudinary.uploader.upload(req.file.path);   //uploads the url from frontend to cloudinary
+      image = result.secure_url;   //secure_url=HTTPS version(safe for browser)
     }
 
     const newPost = await Post.create({
-      Student: req.Student.id,
-      text,
-      image: imageUrl
+      Student: req.Student.id,     //only loggedin student can create post
+      Text,
+      ImageUrl: image
     });
 
-    res.status(201).json(newPost);
+    res.status(201).json(newPost);  //created successfully
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .populate("Student", "Name")    // by using populate,datas would be more structured and name also added(frontend shows name and not just ID)
+      .populate("Comments.Student", "Name")
+      .sort({ createdAt: -1 }); //descending order,i.e latest post first
+
+    res.json(posts);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
